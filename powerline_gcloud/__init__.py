b01620102424b2120e54b7a1684ba0952f7aa855
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2021-10-25 23:58:30 -0700

    support arbitrary shell command

--- powerline_gcloud/__init__.py
+++ powerline_gcloud/__init__.py
@@ -1,5 +1,4 @@
 import subprocess
 
-def project(pl):
-    return u'\u2601 ' + subprocess.Popen(['sh', '-c', 'cat ~/.config/gcloud/configurations/config_default | grep "project =" | cut -d" " -f3'], stdout=subprocess.PIPE).communicate()[0].strip().decode('utf8')
-    return subprocess.Popen(['gcloud', 'config', 'get-value', 'project'], stdout=subprocess.PIPE).communicate()[0].strip().decode('utf8')
+def project(pl, prefix, command):
+    return prefix + (" " if prefix else "") + subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].strip().decode('utf8')
